1b57f05a7aacf6ebfc60a730ee6b224f80d104fd
Hezron Na <hezron.w12@gmail.com>
2023-03-13 14:10:36 +0300

    1

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import tkinter as tk
 
+
+def on_return_press(event):
+    text = event.widget
+    # Get the text that was entered after the Return key was pressed
+    input_text = text.get("end-1c linestart", "end-1c lineend")
+    print("Input text:", input_text)
+
 def main():
     app = tk.Tk()
     app.state("zoomed")
     app.config(bg='gray')
 
+
+    Editor = tk.Text(app)
+    Editor.place(relheight=1, relwidth=1, relx=0, rely=0)
+
+    Editor.bind("<Return>", on_return_press)
 
 
 
