5f0b6590ab070a7c1f02d20101b68476f1812450
Hezron Na <hezron.w12@gmail.com>
2023-03-20 12:05:01 +0300

    1

--- Symbol_Table.py
+++ Symbol_Table.py
@@ -14,7 +14,7 @@ class ParseTreeNode:
 # Define the production rules for the language
 # This is a simplified set of rules for illustration purposes only
 rules = [
-    ('<program>', ['<include-list>' '|' '<declaration>']),
+    ('<program>', ['<include-list>', 'or', '<declaration>']),
     ('<include-list>', ['INCLUDE_DIRECTIVE']),
     ('<declaration>', ['<function_declaration>']),
     ('<function_declaration>', ['<type_specifier>', '<identifier>', 'LEFT_PAREN', '<parameter_list>', 'RIGHT_PAREN', '<compound_statement>']),
@@ -26,31 +26,39 @@ rules = [
 ]
 
 
+
+
 # Define a function that recursively generates a parse tree from the token stream using the production rules
 def parse(tokens, rule):
     node = ParseTreeNode(rule[0])
+    print(rule[0])
     for production in rule[1]:
-        if production.startswith('<'):
-            # If the production is a non-terminal, recursively generate a subtree using the corresponding rule
-            if '|' in production:
-                # If the production contains the "or" operator, try each alternative rule until one succeeds
-                subrules = [r for r in rules if r[0] in production.split('|')]
-                subnode = None
-                for subrule in subrules:
+        print("p", production)
+        if production == 'or':
+            # If the production is a choice between two rules, try both and use the one that succeeds
+            for subrule in rules:
+                if subrule[0] in ('<include-list>', '<declaration>'):
+                    print("s", subrule[0])
                     try:
-                        subnode = parse(tokens, subrule)
+                        child = parse(tokens, subrule)
+                        node.add_child(child)
                         break
                     except ValueError:
                         pass
-                if not subnode:
-                    raise ValueError(f"No valid alternative rules found for production: {production}")
-                node.add_child(subnode)
             else:
-                subrule = next((r for r in rules if r[0] == production), None)
-                if not subrule:
-                    raise ValueError("Invalid production rule: " + production)
-                child = parse(tokens, subrule)
-                node.add_child(child)
+                raise ValueError("No valid subrule found for choice")
+        if production.startswith('<'):
+            # If the production is a non-terminal, recursively generate a subtree using one of the corresponding rules
+            alternatives = [r for r in rules if r[0] == production]
+            for subrule in alternatives:
+                try:
+                    child = parse(tokens, subrule)
+                    node.add_child(child)
+                    break  # If the parse succeeds, stop trying alternatives
+                except ValueError:
+                    pass  # If the parse fails, try the next alternative
+            else:
+                raise ValueError("Invalid production rule: " + production)
         else:
             # If the production is a terminal, consume a token from the token stream and match it against the production
             if not tokens:
@@ -61,8 +69,6 @@ def parse(tokens, rule):
             node.add_child(ParseTreeNode(token))
 
     return node
-
-
 
 
 # Define a function that runs the syntax analyzer on the token stream
