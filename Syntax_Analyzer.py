d8b989d07ddc42c085cf85e4dd92494b945b4929
Hezron Na <hezron.w12@gmail.com>
2023-03-22 22:44:36 +0300

    Committed 2023 0We.224436

--- Syntax_Analyzer.py
+++ Syntax_Analyzer.py
@@ -14,36 +14,7 @@ class ParseTreeNode:
 
 # Define the production rules for the language
 # This is a simplified set of rules for illustration purposes only
-rules = [
-    ('<program>', ['<include-list>',  '<declaration>']),
-    ('<include-list>', ['INCLUDE_DIRECTIVE']),
 
-
-    ('<declaration>', ['<function_declaration>', "<declaration>"]),
-    ('<declaration>', []),
-
-
-
-    ('<function_declaration>', ['<type_specifier>', '<identifier>', 'LEFT_PAREN', '<parameter_list>', 'RIGHT_PAREN', '<compound_statement>']),
-
-    ('<main_function>', ['<type_specifier>', '<identifier>', 'LEFT_PAREN', 'RIGHT_PAREN', '<compound_statement>']),
-
-    ('<type_specifier>', ['KEYWORD']),
-    ('<identifier>', ['IDENTIFIER']),
-    ('<identifier>', ['main_f']),
-
-
-    ('<parameter_list>', ['<parameter>']),
-    ('<parameter_list>', []),
-
-    ('<parameter>', ['<type_specifier>', '<identifier>']),
-
-
-    ('<comma>', ['COMMA']),
-
-
-    ('<compound_statement>', ['LEFT_BRACE', 'RIGHT_BRACE']),
-]
 
 # ('<function_declaration>', ['<type_specifier>', '<identifier>', 'LEFT_PAREN', 'RIGHT_PAREN', '<compound_statement>']),
 
