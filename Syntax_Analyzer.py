6b94fb1f023d039dab46028b919e8fb01a24c9c1
Hezron Na <hezron.w12@gmail.com>
2023-03-22 22:46:41 +0300

    Committed 2023 0We.224641

--- Syntax_Analyzer.py
+++ Syntax_Analyzer.py
@@ -14,6 +14,50 @@ class ParseTreeNode:
 
 # Define the production rules for the language
 # This is a simplified set of rules for illustration purposes only
+rules = [
+    ('<program>', ['<external-declaration>']),
+    ('<program>', ['<program>', '<external-declaration>']),
+    ('<external-declaration>', ['<function-definition>']),
+    ('<external-declaration>', ['<declaration>']),
+    ('<function-definition>', ['<declaration-specifiers>', '<declarator>', '<declaration-list>', '<compound-statement>']),
+    ('<declaration>', ['<declaration-specifiers>', '<init-declarator-list>', ';']),
+    ('<declaration-specifiers>', ['<type-specifier>']),
+    ('<declaration-specifiers>', ['<type-specifier>', '<declaration-specifiers>']),
+    ('<declarator>', ['<direct-declarator>']),
+    ('<declarator>', ['<pointer>', '<direct-declarator>']),
+    ('<direct-declarator>', ['<identifier>']),
+    ('<direct-declarator>', ['(', '<declarator>', ')']),
+    ('<direct-declarator>', ['<direct-declarator>', '[', '<constant-expression>', ']']),
+    ('<direct-declarator>', ['<direct-declarator>', '(', '<parameter-type-list>', ')']),
+    ('<direct-declarator>', ['<direct-declarator>', '(', '<identifier-list>', ')']),
+    ('<direct-declarator>', ['<direct-declarator>', '(', ')']),
+    ('<parameter-type-list>', ['<parameter-list>']),
+    ('<parameter-type-list>', ['<parameter-list>', ',', '...']),
+    ('<parameter-list>', ['<parameter-declaration>']),
+    ('<parameter-list>', ['<parameter-list>', ',', '<parameter-declaration>']),
+    ('<parameter-declaration>', ['<declaration-specifiers>', '<declarator>']),
+    ('<init-declarator-list>', ['<init-declarator>']),
+    ('<init-declarator-list>', ['<init-declarator-list>', ',', '<init-declarator>']),
+    ('<init-declarator>', ['<declarator>']),
+    ('<init-declarator>', ['<declarator>', '=', '<initializer>']),
+    ('<initializer>', ['<assignment-expression>']),
+    ('<initializer>', ['{', '<initializer-list>', '}']),
+    ('<initializer>', ['{', '<initializer-list>', ',', '}']),
+    ('<initializer-list>', ['<designation>', '<initializer>']),
+    ('<initializer-list>', ['<initializer>']),
+    ('<initializer-list>', ['<initializer-list>', ',', '<designation>', '<initializer>']),
+    ('<initializer-list>', ['<initializer-list>', ',', '<initializer>']),
+    ('<designation>', ['<designator-list>', '=']),
+    ('<designator-list>', ['<designator>']),
+    ('<designator-list>', ['<designator-list>', '<designator>']),
+    ('<designator>', ['[', '<constant-expression>', ']']),
+    ('<designator>', ['.', '<identifier>']),
+    ('<type-specifier>', ['void']),
+    ('<type-specifier>', ['char']),
+    ('<type-specifier>', ['short']),
+    ('<type-specifier>', ['int'])
+]
+
 
 
 # ('<function_declaration>', ['<type_specifier>', '<identifier>', 'LEFT_PAREN', 'RIGHT_PAREN', '<compound_statement>']),
