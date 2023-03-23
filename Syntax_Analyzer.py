59877d3f5c63bf5b2db2cd2478bbf953291ebb81
Hezron Na <hezron.w12@gmail.com>
2023-03-23 08:51:50 +0300

    Committed 2023 0Th. 85150

--- Syntax_Analyzer.py
+++ Syntax_Analyzer.py
@@ -20,7 +20,7 @@ rules = [
 
 
     ('<declaration>', ['<function_declaration>', "<declaration>"]),
-    ('<declaration>', ['<var_declaration>']),
+    ('<declaration>', ['<var_declaration>', '<declaration>']),
     ('<declaration>', []),
 
     ('<function_declaration>', ['<type_specifier>', '<identifier>', 'LEFT_PAREN', '<parameter_list>', 'RIGHT_PAREN', '<compound_statement>']),
