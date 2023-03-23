b1a9c23e25bae8c5bd4329ae7f5dfc85cc17eb5a
Hezron Na <hezron.w12@gmail.com>
2023-03-23 16:20:55 +0300

    Committed 2023 0Th.162054

--- Semantic_Analyzer.py
+++ Semantic_Analyzer.py
@@ -2,103 +2,85 @@ import lexical_Analyzer
 
 tokens = lexical_Analyzer.lex('program.c')
 
+# Define a class to represent a node in the parse tree
+class ParseTreeNode:
+    def __init__(self, value, children=None):
+        self.value = value
+        self.children = children if children else []
+
+    def add_child(self, child):
+        self.children.append(child)
+
+
 # Define the production rules for the language
-# This is just an example, you'll need to define your own rules for your specific language
-# Each rule is represented as a tuple where the first element is the name of the nonterminal symbol
-# and the remaining elements are the symbols in the production
+# This is a simplified set of rules for illustration purposes only
 rules = [
-    ('program', 'declaration_list'),
-    ('declaration_list', 'declaration'),
-    ('declaration_list', 'declaration_list declaration'),
-    ('declaration', 'variable_declaration'),
-    ('declaration', 'function_declaration'),
-    ('variable_declaration', 'type ID ;'),
-    ('function_declaration', 'type ID ( parameter_list ) compound_statement'),
-    ('parameter_list', 'parameter'),
-    ('parameter_list', 'parameter_list , parameter'),
-    ('parameter', 'type ID'),
-    ('type', 'int'),
-    ('type', 'float'),
-    ('type', 'bool'),
-    ('compound_statement', '{ statement_list }'),
-    ('statement_list', 'statement'),
-    ('statement_list', 'statement_list statement'),
-    ('statement', 'expression_statement'),
-    ('statement', 'variable_declaration'),
-    ('statement', 'if_statement'),
-    ('statement', 'while_statement'),
-    ('expression_statement', 'expression ;'),
-    ('if_statement', 'if ( expression ) statement'),
-    ('if_statement', 'if ( expression ) statement else statement'),
-    ('while_statement', 'while ( expression ) statement'),
-    ('expression', 'ID = expression'),
-    ('expression', 'expression + expression'),
-    ('expression', 'expression - expression'),
-    ('expression', 'expression * expression'),
-    ('expression', 'expression / expression'),
-    ('expression', '( expression )'),
-    ('expression', 'ID'),
-    ('expression', 'NUMBER'),
-    ('expression', 'BOOLEAN'),
+    ('<parameter_list>', ['<parameter>', '<parameter_list>']),
+    ('<parameter_list>', []),
+
+    ('<parameter>', ['<type_specifier>', '<identifier>']),
+    ('<parameter2>', ['COMMA', '<type_specifier>', '<identifier>']),
+
+    ('<comma>', ['COMMA']),
+
+    ('<BLOCK>', ['LEFT_BRACE',  'RIGHT_BRACE']),
+    ('<type_specifier>', ['KEYWORD']),
+    ('<identifier>', ['IDENTIFIER']),
+
 ]
 
-# Define a function to perform syntax analysis on a list of tokens
-def parse(tokens):
-    tree = []
-    position = 0
 
-    # Start with the start symbol of the grammar
-    symbol = 'program'
 
-    # Define a function to match a token to a terminal symbol
-    def match(token, symbol):
-        return token[0] == symbol
+# ('<function_declaration>', ['<type_specifier>', '<identifier>', 'LEFT_PAREN', 'RIGHT_PAREN', '<compound_statement>']),
 
-    # Define a function to generate a subtree from a production rule
-    def generate_subtree(rule, children):
-        return (rule[0], children)
-
-    # Define a function to recursively parse a nonterminal symbol
-    def parse_nonterminal(symbol):
-        nonlocal position
-        children = []
-
-        # Try each production rule for the symbol
-        for rule in rules:
-            if rule[0] == symbol:
-                print(12)
-                for symbol in rule[1:]:
-                    # If the symbol is a nonterminal, recursively parse it
-                    if symbol.isupper():
-                        subtree = parse_nonterminal(symbol)
-                        if subtree:
-                            children.append(subtree)
-                            continue
-
-                    # If the symbol is a terminal, match it with the current token
-                    elif match(tokens[position], symbol):
-                        children.append(tokens[position])
-                        position += 1
-                        continue
-
-                    # If the symbol does not match, backtrack and try the next production rule
+# Define a function that recursively generates a parse tree from the token stream using the production rules
+def parse(tokens, rule):
+    node = ParseTreeNode(rule[0])
+    print(rule[0])
+    for production in rule[1]:
+        if production.startswith('<'):
+            # If the production is a non-terminal, recursively generate a subtree using the corresponding rule
+            subrules = [r for r in rules if r[0] == production]
+            if not subrules:
+                raise ValueError("Invalid production rule: " + production)
+            match_found = False
+            for subrule in subrules:
+                try:
+                    child = parse(tokens, subrule)
+                    node.add_child(child)
+                    match_found = True
+                    print('\t \t Match',subrule )
                     break
+                except ValueError:
+                    pass
+            if not match_found:
+                try:
+                    token = tokens
+                    #print(f"\n===========================================================================")
+                    print(f"Expected token type {production}, got {token[0]}: {token[1]}")
+                    print("No matching subrule found for production rule: ", production)
+                    raise ValueError("No matching subrule found for production rule: ", production)
+                except:
+                    raise ValueError("No matching subrule found for production rule: ", production)
+        else:
+            # If the production is a terminal, consume a token from the token stream and match it against the production
+            if not tokens:
+                raise ValueError("Unexpected end of input")
+            token = tokens.pop(0)
+            if token[0] != production:
+                raise ValueError(f"Expected token type {production}, got {token[0]}: {token[1]}")
+            node.add_child(ParseTreeNode(token))
 
-                # If all symbols match, generate a subtree and return it
-                else:
-                    return generate_subtree(rule, children)
+    return node
 
-        # If no production rule matches, return None to indicate a syntax error
-        return None
-
-    # Start parsing with the start symbol
-    tree = parse_nonterminal(symbol)
-    print(tree)
-    # If the parse was successful, make sure we consumed all the tokens
-    if tree and position == len(tokens):
-        print('fail')
-        return tree
+# Define a function that runs the syntax analyzer on the token stream
+def syntax_analyze(tokens):
+    tree = parse(tokens, rules[0])
+    if tokens:
+        raise ValueError("Unexpected tokens at end of input")
+    return tree
 
 
-x = parse(tokens)
-print(x)
+print(tokens)
+tree = syntax_analyze(tokens)
+print("tree", tree.value)
