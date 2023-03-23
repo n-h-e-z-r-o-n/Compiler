05042238f0f4f6b6d52b1310c1ba0b75f6bde939
Hezron Na <hezron.w12@gmail.com>
2023-03-23 21:31:58 +0300

    Committed 2023 0Th.213158

--- Semantic_Analyzer.py
+++ Semantic_Analyzer.py
@@ -22,69 +22,45 @@ rules = [
 
 
 
+# First, create the predictive parsing table
+parsing_table = {}
+for non_terminal, productions in rules:
+    for lookahead in set(prod[0] for prod in productions if not prod.startswith('<')):
+        parsing_table.setdefault(non_terminal, {})[lookahead] = [prod for prod in productions if prod.startswith(lookahead)]
 
-class ParseTable:
-    def __init__(self, rules):
-        self.table = {}
-        for nonterminal, productions in rules:
-            for lookahead in self.get_first_set(productions):
-                if lookahead not in self.table:
-                    self.table[lookahead] = {}
-                self.table[lookahead][nonterminal] = productions
-
-    def get_first_set(self, symbols):
-        first_set = set()
-        for symbol in symbols:
-            if symbol.startswith('<'):
-                first_set.update(self.get_first_set(self.table.get(symbol, {})))
-                if ('EPSILON' not in first_set):
-                    break
-            else:
-                first_set.add(symbol)
-                break
-        else:
-            first_set.add('EPSILON')
-        return first_set
-
-
-def parse(tokens, start_symbol, parse_table):
-    stack = [start_symbol]
-    tree_stack = [ParseTreeNode(start_symbol)]
-    lookahead = tokens[0][0] if tokens else None
+# Then define the predictive parse function
+def predictive_parse(tokens):
+    stack = ['$', '<program>']
+    token_index = 0
+    tree = ParseTreeNode('<program>')
+    node_stack = [tree]
 
     while stack:
-        symbol = stack.pop()
-        tree = tree_stack.pop()
+        symbol = stack[-1]
+        token_type = tokens[token_index][0] if token_index < len(tokens) else '$'
 
         if symbol.startswith('<'):
-            production = parse_table.table[lookahead].get(symbol, None)
-            if production is None:
-                raise ValueError(f"Unexpected token: {lookahead}")
-            for rhs_symbol in reversed(production):
-                stack.append(rhs_symbol)
-                if rhs_symbol.startswith('<'):
-                    tree.add_child(ParseTreeNode(rhs_symbol))
-                    tree_stack.append(tree.children[-1])
-                else:
-                    if not tokens:
-                        raise ValueError("Unexpected end of input")
-                    token = tokens.pop(0)
-                    if token[0] != rhs_symbol:
-                        raise ValueError(f"Expected token type {rhs_symbol}, got {token[0]}: {token[1]}")
-                    tree.add_child(ParseTreeNode(token))
-                    lookahead = tokens[0][0] if tokens else None
-
+            # Non-terminal symbol
+            if token_type not in parsing_table[symbol]:
+                raise ValueError(f"Unexpected token type {token_type}: {tokens[token_index][1]}")
+            production = parsing_table[symbol][token_type][0]
+            node = ParseTreeNode(production)
+            node_stack[-1].add_child(node)
+            node_stack.append(node)
+            stack.pop()
+            for s in reversed(production.split()[::-1]):
+                if s != '<epsilon>':
+                    stack.append(s)
+        elif symbol == token_type:
+            # Terminal symbol
+            node = ParseTreeNode(tokens[token_index])
+            node_stack[-1].add_child(node)
+            node_index += 1
+            stack.pop()
         else:
-            if not tokens:
-                raise ValueError("Unexpected end of input")
-            token = tokens.pop(0)
-            if token[0] != symbol:
-                raise ValueError(f"Expected token type {symbol}, got {token[0]}: {token[1]}")
-            tree.add_child(ParseTreeNode(token))
-            lookahead = tokens[0][0] if tokens else None
+            raise ValueError(f"Expected symbol {symbol}, got token type {token_type}: {tokens[token_index][1]}")
 
     return tree
-
 
 
 table = ParseTable(rules)
