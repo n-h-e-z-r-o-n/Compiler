aa7e4a5c8be2cc6d5fa7630afd4c0fc978fdd570
Hezron Na <hezron.w12@gmail.com>
2023-03-25 20:18:42 +0300

    Committed 2023 0Sa.201842

--- Error_Handler.py
+++ Error_Handler.py
@@ -24,8 +24,9 @@ rules = [
 
     ('<function_declaration>', ['<type_specifier>', '<identifier>', '<parameter_list>',  '<compound_statement>']),
 
+    #('<function_declaration_closure>', ['<type_specifier>', '<identifier>', '<parameter_list>',  '<compound_statement>']),
 
-    ('<parameter_list>', ['LEFT_PAREN', '<type_specifier>', '<identifier>', '<more_parameters>', 'RIGHT_PAREN']),
+    ('<parameter_list>', ['LEFT_PAREN', '<type_specifier>', '<identifier>', 'RIGHT_PAREN']),
     ('<more_parameters>', ['COMMA', '<type_specifier>', '<identifier>', '<more_parameters>']),
     ('<more_parameters>', []),
 
@@ -46,6 +47,7 @@ rules = [
 
 # ('<function_declaration>', ['<type_specifier>', '<identifier>', 'LEFT_PAREN', 'RIGHT_PAREN', '<compound_statement>']),
 
+# Define a function that recursively generates a parse tree from the token stream using the production rules
 def parse(tokens, rule, kleene_dict=None):
     node = ParseTreeNode(rule[0])
     print(rule[0])
@@ -63,6 +65,8 @@ def parse(tokens, rule, kleene_dict=None):
                     print('\t \t Match', subrules[0])
                 except ValueError:
                     break
+                if not tokens:
+                    break  # Added check for end of input
         elif production.startswith('<'):
             # If the production is a non-terminal, recursively generate a subtree using the corresponding rule
             subrules = [r for r in rules if r[0] == production]
@@ -101,6 +105,8 @@ def parse(tokens, rule, kleene_dict=None):
                 print('\t \t Kleene closure')
             except ValueError:
                 break
+            if not tokens:
+                break  # Added check for end of input
 
     return node
 
