c7246ed5a51cefedecee4666db66edab86c60cdf
Hezron Na <hezron.w12@gmail.com>
2023-03-25 17:08:18 +0300

    Committed 2023 0Sa.170818

--- Semantic_Analyzer.py
+++ Semantic_Analyzer.py
@@ -21,7 +21,7 @@ rules = [
     ('<declaration>', ['<function_declaration>']),
     ('<declaration>', []),
 
-    ('<function_declaration>', ['<type_specifier>', '<identifier>', 'LEFT_PAREN', 'RIGHT_PAREN', '<compound_statement>', '<function_declaration>*']),
+    ('<function_declaration>', ['<type_specifier>', '<identifier>', 'LEFT_PAREN', 'RIGHT_PAREN', '<compound_statement>']),
 
     ('<function_declaration>*', ['<function_declaration>']),
     ('<function_declaration>*', []),
