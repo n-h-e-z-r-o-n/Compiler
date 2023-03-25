ac10bb637fd853c6477b542cc93eca889416c535
Hezron Na <hezron.w12@gmail.com>
2023-03-25 20:47:40 +0300

    Committed 2023 0Sa.204739

--- Error_Handler.py
+++ Error_Handler.py
@@ -21,10 +21,7 @@ rules = [
     ('<declaration>', ['<function_declaration>*']),
     ('<declaration>', []),
 
-
     ('<function_declaration>', ['<type_specifier>', '<identifier>', '<parameter_list>',  '<compound_statement>']),
-
-    #('<function_declaration_closure>', ['<type_specifier>', '<identifier>', '<parameter_list>',  '<compound_statement>']),
 
     ('<parameter_list>', ['LEFT_PAREN', '<type_specifier>', '<identifier>', 'RIGHT_PAREN']),
     ('<more_parameters>', ['COMMA', '<type_specifier>', '<identifier>', '<more_parameters>']),
@@ -32,7 +29,7 @@ rules = [
 
     ('<type_specifier>', ['KEYWORD']),
     ('<identifier>', ['IDENTIFIER']),
-    ('<identifier>', ['main_f']),
+
 
     ('<comma>', ['COMMA']),
     ('<compound_statement>', ['LEFT_BRACE', 'RIGHT_BRACE']),
