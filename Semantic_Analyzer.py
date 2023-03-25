7a79d6f059b296f59ba0a6343a6171937f3806ec
Hezron Na <hezron.w12@gmail.com>
2023-03-25 16:28:36 +0300

    Committed 2023 0Sa.162836

--- Semantic_Analyzer.py
+++ Semantic_Analyzer.py
@@ -1,39 +1,56 @@
-# Define the grammar
+import lexical_Analyzer
+
+tokens = lexical_Analyzer.lex('program.c')
+
+# Define a class to represent a node in the parse tree
+class ParseTreeNode:
+    def __init__(self, value, children=None):
+        self.value = value
+        self.children = children if children else []
+
+    def add_child(self, child):
+        self.children.append(child)
+
+
+# Define the production rules for the language
+# This is a simplified set of rules for illustration purposes only
 rules = [
-    ('<expr>', ['<term>', '+', '<term>']),
-    ('<expr>', ['<term>']),
-    ('<term>', ['<factor>', '*', '<factor>']),
-    ('<term>', ['<factor>']),
-    ('<factor>', ['NUMBER']),
-    ('<factor>', ['(', '<expr>', ')']),
+    ('<program>', ['<include-list>',  '<declaration>']),
+    ('<include-list>', ['INCLUDE_DIRECTIVE']),
+
+    ('<declaration>', ['<function_declaration>']),
+    ('<declaration>', []),
+
+    #('<function_declaration>', ['<type_specifier>', '<identifier>', 'LEFT_PAREN', 'RIGHT_PAREN', '<compound_statement>']),
+    ('<function_declaration>', ['<type_specifier>', '<identifier>',  '<parameter_list>', '<compound_statement>']),
+
+    ('<type_specifier>', ['KEYWORD']),
+    ('<identifier>', ['IDENTIFIER']),
+    ('<identifier>', ['main_f']),
+
+    ('<comma>', ['COMMA']),
+    ('<compound_statement>', ['LEFT_BRACE', 'RIGHT_BRACE']),
+
+    #('<parameter_list>', ['LEFT_PAREN', 'RIGHT_PAREN']),
+    ('<parameter_list>', ['LEFT_PAREN', '<type_specifier>', '<identifier>', '<parameter>']),
+    ('<parameter>', ['COMMA', '<type_specifier>', '<identifier>', '<parameter>']),
+    ('<parameter>', []),
+
 ]
 
-# Parse a simple expression: 2 * (3 + 4)
-tokens = [('NUMBER', '2'), ('*', ''), ('(', ''), ('NUMBER', '3'), ('+', ''), ('NUMBER', '4'), (')', '')]
 
-# Define the parse tree node class
-class ParseTreeNode:
-    def __init__(self, value):
-        self.value = value
-        self.children = []
 
-    def add_child(self, node):
-        self.children.append(node)
 
-# Define the parse function
+
+
+# ('<function_declaration>', ['<type_specifier>', '<identifier>', 'LEFT_PAREN', 'RIGHT_PAREN', '<compound_statement>']),
+
+# Define a function that recursively generates a parse tree from the token stream using the production rules
 def parse(tokens, rule):
     node = ParseTreeNode(rule[0])
+    print(rule[0])
     for production in rule[1]:
-        if production.endswith('*'):
-            # If the production ends with '*', apply the Kleene closure
-            subrule = [production[:-1]]
-            while True:
-                try:
-                    child = parse(tokens, subrule)
-                    node.add_child(child)
-                except ValueError:
-                    break
-        elif production.startswith('<'):
+        if production.startswith('<'):
             # If the production is a non-terminal, recursively generate a subtree using the corresponding rule
             subrules = [r for r in rules if r[0] == production]
             if not subrules:
@@ -44,27 +61,35 @@ def parse(tokens, rule):
                     child = parse(tokens, subrule)
                     node.add_child(child)
                     match_found = True
+                    print('\t \t Match',subrule )
                     break
-                except ValueError:
-                    pass
+                except ValueError as e:
+                    print('r', e)
+
             if not match_found:
-                raise ValueError("No matching subrule found for production rule: " + production)
+                    raise ValueError("No matching subrule found for production rule: ", production)
+
         else:
             # If the production is a terminal, consume a token from the token stream and match it against the production
             if not tokens:
                 raise ValueError("Unexpected end of input")
+
             token = tokens.pop(0)
+            print('============',token)
             if token[0] != production:
                 raise ValueError(f"Expected token type {production}, got {token[0]}: {token[1]}")
             node.add_child(ParseTreeNode(token))
+
     return node
 
-# Parse the input tokens using the <expr> rule
-tree = parse(tokens, ('<expr>',))
+# Define a function that runs the syntax analyzer on the token stream
+def syntax_analyze(tokens):
+    tree = parse(tokens, rules[0])
+    if tokens:
+        raise ValueError("Unexpected tokens at end of input")
+    return tree
 
-# Print the parse tree
-def print_tree(node, indent=0):
-    print('  ' * indent + str(node.value))
-    for child in node.children:
-        print_tree(child, indent + 1)
-print_tree(tree)
+
+print('\nTOKENS\n\t', tokens)
+tree = syntax_analyze(tokens)
+print("tree", tree.value)
