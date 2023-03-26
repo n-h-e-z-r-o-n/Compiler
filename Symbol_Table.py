aff3efedf8970c0180f339aae7aae061540b6f91
Hezron Na <hezron.w12@gmail.com>
2023-03-26 22:18:45 +0300

    Committed 2023 0Su.221844

--- Symbol_Table.py
+++ Symbol_Table.py
@@ -30,19 +30,36 @@ rules = [
 
 ]
 
-def praser(tokens, rules):
-    pass
-print(rules[0])
-print(len(rules[1]))
 
+def syntax_analyzer(tokens, rules):
+    stack = ['$']
+    input_list = [token[0] for token in tokens] + ['$']
+    input_index = 0
+    current_production = ('<program>', [])
 
-
-def parse(tokens, rule):
-    for production in rule[1]:
-        if production.startswith('<'):
-            print('=== ', production)
-
+    while True:
+        top_of_stack = stack[-1]
+        if top_of_stack in ('$', '<declaration>', '<function_declaration>', '<parameter_list>', '<more_parameters>', '<type_specifier>', '<identifier>', '<comma>', '<compound_statement>'):
+            stack.pop()
+            current_production[1].append(top_of_stack)
+            for rule in rules:
+                if rule[0] == top_of_stack and current_production[1] == rule[1]:
+                    current_production = rule
+                    break
+        elif top_of_stack == input_list[input_index]:
+            stack.pop()
+            input_index += 1
         else:
-            pass
+            # There is a syntax error
+            return False
 
-parse(tokens, rules)
+        if current_production[1] == []:
+            if current_production[0] == '<program>':
+                # The input is syntactically correct
+                return True
+            else:
+                stack.append(current_production[0])
+                current_production = ('', [])
+        else:
+            for symbol in reversed(current_production[1]):
+                stack.append(symbol)
