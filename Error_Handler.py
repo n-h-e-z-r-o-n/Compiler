3fb72044b97ee6c69c89b9dfe6c0c1f497b5a7bd
Hezron Na <hezron.w12@gmail.com>
2023-03-26 14:52:13 +0300

    Committed 2023 0Su.145213

--- Error_Handler.py
+++ Error_Handler.py
@@ -1,115 +1,96 @@
-import lexical_Analyzer
+import re
 
-tokens = lexical_Analyzer.lex('program.c')
-
-# Define a class to represent a node in the parse tree
-class ParseTreeNode:
-    def __init__(self, value, children=None):
-        self.value = value
-        self.children = children if children else []
-
-    def add_child(self, child):
-        self.children.append(child)
+symbol_table = {}
 
 
-# Define the production rules for the language
-# This is a simplified set of rules for illustration purposes only
-rules = [
-    ('<program>', ['<include-list>',  '<declaration>']),
-    ('<include-list>', ['INCLUDE_DIRECTIVE']),
-
-    ('<declaration>', ['<function_declaration>*']),
-    ('<declaration>', []),
-
-
-    ('<function_declaration>', ['<type_specifier>', '<identifier>', '<parameter_list>',  '<compound_statement>']),
-
-    ('<parameter_list>', ['LEFT_PAREN', '<type_specifier>', '<identifier>', 'RIGHT_PAREN']),
-    ('<more_parameters>', ['COMMA', '<type_specifier>', '<identifier>', '<more_parameters>']),
-    ('<more_parameters>', []),
-
-    ('<type_specifier>', ['KEYWORD']),
-    ('<identifier>', ['IDENTIFIER']),
-    ('<identifier>', ['main_f']),
-
-    ('<comma>', ['COMMA']),
-    ('<compound_statement>', ['LEFT_BRACE', 'RIGHT_BRACE']),
-
-
+# Define regular expression patterns for different types of tokens(assigning tokens to lexemes)
+patterns = [
+    (r'#include\s+<.*?>', 'INCLUDE_DIRECTIVE'),
+    (r'\b(int|void|char|bool|float|long|return)\b', 'KEYWORD'),
+    (r'\b(if)\b', 'if'),
+    (r'\b(true|false|1|0)\b', 'BOOLEAN'),
+    (r'\b\d+\.\d+\b', 'floating_point'),
+    (r'\b\d+\b', 'integer'),
+    (r"'.'", 'char'),
+    (r'\b([a-zA-Z_][a-zA-Z0-9_]*)\b', 'IDENTIFIER'),
+    (r'\+', 'PLUS'),
+    (r'-', 'MINUS'),
+    (r'\*', 'MULTIPLY'),
+    (r'(\/\/[^\n\r]*[\n\r])|\/\*[\s\S]*?\*\/', 'COMMENT'),
+    (r'/(?![\/\*])[\n\s]*', 'DIVIDE'),
+    (r'%', 'MODULUS'),
+    (r'=', 'ASSIGN'),
+    (r'==', 'EQUAL'),
+    (r'!=', 'NOT_EQUAL'),
+    (r'<', 'LESS_THAN'),
+    (r'>', 'GREATER_THAN'),
+    (r'<=', 'LESS_THAN_EQUAL'),
+    (r'>=', 'GREATER_THAN_EQUAL'),
+    (r'\(', 'LEFT_PAREN'),
+    (r'\)', 'RIGHT_PAREN'),
+    (r'\{', 'LEFT_BRACE'),
+    (r'\}', 'RIGHT_BRACE'),
+    (r';', 'SEMICOLON'),
+    (r',', 'COMMA'),
 ]
 
-# Define a function that recursively generates a parse tree from the token stream using the production rules
-def parse(tokens, rule, kleene_dict=None):
-    node = ParseTreeNode(rule[0])
-    print(rule[0])
-    for production in rule[1]:
-        if production.endswith('*'):
-            # If the production is a Kleene closure of a non-terminal
-            non_terminal = production[:-1]
-            subrules = [r for r in rules if r[0] == non_terminal]
-            if not subrules:
-                raise ValueError("Invalid production rule: " + non_terminal)
-            while True:
-                try:
-                    child = parse(tokens, subrules[0], kleene_dict)
-                    node.add_child(child)
-                    print('\t \t Match', subrules[0])
-                except ValueError:
-                    break
-                if not tokens:
-                    break  # Added check for end of input
-        elif production.startswith('<'):
-            # If the production is a non-terminal, recursively generate a subtree using the corresponding rule
-            subrules = [r for r in rules if r[0] == production]
-            if not subrules:
-                raise ValueError("Invalid production rule: " + production)
-            match_found = False
-            for subrule in subrules:
-                try:
-                    child = parse(tokens, subrule, kleene_dict)
-                    node.add_child(child)
-                    match_found = True
-                    print('\t \t Match', subrule)
-                    break
-                except ValueError as e:
-                    print('r', e)
 
-            if not match_found:
-                raise ValueError("No matching subrule found for production rule: ", production)
-        else:
-            # If the production is a terminal, consume a token from the token stream and match it against the production
-            if not tokens:
-                raise ValueError("Unexpected end of input")
+# Define a function that reads a program from a text file and generates a list of tokens
+def lex(filename):
+    with open(filename, 'r') as f:
+        program = f.read()
+    tokens = []
+    position = 0
+    while position < len(program):
+        match = None
 
-            token = tokens.pop(0)
-            print('============', token)
-            if token[0] != production:
-                raise ValueError(f"Expected token type {production}, got {token[0]}: {token[1]}")
-            node.add_child(ParseTreeNode(token))
+        # Skip over empty lines
+        if program[position] == '\n':
+            position += 1
+            continue
 
-    # Handle Kleene closure specified in the rule
-    if kleene_dict and rule[0] in kleene_dict:
-        while True:
-            try:
-                child = parse(tokens, rule, kleene_dict=None)
-                node.add_child(child)
-                print('\t \t Kleene closure')
-            except ValueError:
+        # Skip over whitespace
+        if re.match(r'\s', program[position]):
+            position += 1
+            continue
+
+        for pattern, token_type in patterns:
+            regex = re.compile(pattern)
+            match = regex.match(program, position)
+
+            # get rid of code comments
+            if match:
+                if token_type != 'COMMENT': # the tokenization logic skips over comment tokens
+                    tokens.append((token_type, match.group(0)))
+                position = match.end(0)
                 break
-            if not tokens:
-                break  # Added check for end of input
 
-    return node
+        if not match:
+            print("Illegal character: " + program[position])
+            position += 1
 
+    return tokens
 
-# Define a function that runs the syntax analyzer on the token stream
-def syntax_analyze(tokens):
-    tree = parse(tokens, rules[0])
-    if tokens:
-        raise ValueError("Unexpected tokens at end of input")
-    return tree
+# Example usage
+tokens = lex('program.c')
 
 
-print('\nTOKENS\n\t', tokens)
-tree = syntax_analyze(tokens)
-print("tree", tree)
+# ============================================================================================================
+def generate_symbol_table(tokens):
+    symbol_table = {}
+
+    for token_type, value in tokens:
+        if token_type == 'IDENTIFIER':
+            if value not in symbol_table:
+                symbol_table[value] = {
+                    'type': None,
+                    'value': None,
+                    'line_number': None
+                }
+
+    return symbol_table
+
+symbol_table = generate_symbol_table(tokens)
+print("\nSYMBOL_TABLE \n\t", symbol_table)
+# ============================================================
+
