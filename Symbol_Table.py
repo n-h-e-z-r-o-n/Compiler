967a67e157f6036ea99445e1aa040fa281aec2fe
Hezron Na <hezron.w12@gmail.com>
2023-03-27 22:44:24 +0300

    Committed 2023 0Mo.224424

--- Symbol_Table.py
+++ Symbol_Table.py
@@ -1,4 +1,4 @@
-
+"""
 import json
 # Data to be written
 dictionary ={
@@ -12,7 +12,7 @@ with open('test.json') as json_file:
     data = json.load(json_file)
 
 temp = data['Symbol_table']
-temp.delete(dictionary)
+temp.append(dictionary)
 with open('test.json', 'w') as json_file_write:
    json.dump(data,json_file_write, indent=4)
 
@@ -24,5 +24,11 @@ print(temp)
 for i in range(len(temp)):
     print(data['Symbol_table'][i]['IDENTIFIER'])
     data['Symbol_table'][i]['IDENTIFIER'] = '34'
+"""
+import json
 
+data = {"Symbol_table": []}
 
+# Open the JSON file in write mode and write the empty object to it
+with open("test.json", 'w') as f:
+    json.dump(data, f)
