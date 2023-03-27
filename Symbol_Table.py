0d80bddc93743ef720a3495110ac2c10beb444e4
Hezron Na <hezron.w12@gmail.com>
2023-03-27 15:40:28 +0300

    Committed 2023 0Mo.154028

--- Symbol_Table.py
+++ Symbol_Table.py
@@ -10,8 +10,17 @@ dictionary ={
 
 with open('test.json') as json_file:
     data = json.load(json_file)
-
+"""
 temp = data['Symbol_table']
 temp.append(dictionary)
 with open('test.json', 'w') as json_file_write:
    json.dump(data,json_file_write, indent=4)
+"""
+
+
+temp =  data['Symbol_table'][1]['IDENTIFIER']
+
+print(temp)
+
+
+
