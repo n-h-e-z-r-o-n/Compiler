f01b54dd3c45c7c269a96275701975c0b9913fe1
Hezron Na <hezron.w12@gmail.com>
2023-04-01 01:34:18 +0300

    Committed 2023 0Sa. 13418

--- Error_Handler.py
+++ Error_Handler.py
@@ -5,19 +5,8 @@ from parsimonious.nodes import NodeVisitor
 grammar = Grammar(
     """
     program = (declaration / statement)*
-    declaration = type identifier ";"
-    type = "int" / "float" / "char" / "bool"
+    declaration = identifier
     identifier = ~"[a-zA-Z_][a-zA-Z0-9_]*"
-    statement = expression ";"
-    expression = term (add_op term)*
-    term = factor (mul_op factor)*
-    factor = identifier / integer / float / char / bool / "(" expression ")"
-    integer = ~"[0-9]+"
-    float = ~"[0-9]+.[0-9]+"
-    char = ~"'[a-zA-Z0-9_?]*[\s]*'"
-    bool = "true" / "false"
-    add_op = "+" / "-"
-    mul_op = "*" / "/" / "%"
     """
 )
 
@@ -34,12 +23,6 @@ def parse_tokens(tokens):
     # Use the visitor class to traverse the parse tree and generate the output
 
 
-    return output
-
-
 import lexical_Analyzer
-import json
-tokens = lexical_Analyzer.lexical_analyzer('program.c')
 
-#output = parse_tokens(tokens)
-#print(json.dumps(output, indent=4))
+tokens = lexical_Analyzer.lexical_analyzer('program.c')
