93b0a80f6bfa07aaf349c7fadaceed740df9a5a0
Hezron Na <hezron.w12@gmail.com>
2023-04-20 00:17:14 +0300

    Committed 2023 0Th. 01714

--- lexical_Analyzer.py
+++ lexical_Analyzer.py
@@ -6,7 +6,6 @@ import json  # for symbol table {access and storage}
 
 
 # Define regular expression patterns for different types of tokens(assigning tokens to lexemes)
-
 patterns_rg = [
     (r'#include', 'INCLUDE_ID'),
     (r'<[A-Za-z]+.h>', 'INCLUDE_DIRECTIVE'),
@@ -151,4 +150,4 @@ def generate_symbol_table(token_list):
                 json.dump(data, json_file_write, indent=4)
 
 
-generate_symbol_table(tokens)
+#generate_symbol_table(tokens)
