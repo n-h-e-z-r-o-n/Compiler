109ff8266246e9fa61c4fad325b5aff4ca7e967b
Hezron Na <hezron.w12@gmail.com>
2023-04-25 20:09:03 +0300

    Committed 2023 0Tu.200903

--- Parser/parser.py
+++ Parser/parser.py
@@ -273,10 +273,11 @@ def statments(token, postion):  # statement: (declaration | initializing | funct
                 else:
                     # print(F"WHILE-STATEMENT: {while_key} {wh_lp} <error incomplete-while-statement>")
                     statment_block += f"\n\t\t\t\t\tWHILE-STATEMENT: {while_key} {wh_lp} <error incomplete-while-statement>"
+                    Error_list += "\nSyntax error: while statment <error incomplete-while-statement> <missing ')'...>  at line " + tokens[current_token - 1][2]
             else:
                 # print(F"WHILE-STATEMENT: {while_key}  <error incomplete-while-statement>")
                 statment_block += f"\n\t\t\t\t\tWHILE-STATEMENT: {while_key}  <error incomplete-while-statement>"
-                Error_list += "\nSyntax error: while statment incomplete at line ', tokens[current_token][2]"
+                Error_list += "\nSyntax error: while statment incomplete at line " + tokens[current_token][2]
 
         elif current_token  < len(tokens) and tokens[current_token][0] == 'IDENTIFIER':
             name = tokens[current_token][1]
