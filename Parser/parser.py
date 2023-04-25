57c15606bd42a381e1b69da36e25db99dc755bef
Hezron Na <hezron.w12@gmail.com>
2023-04-25 14:56:27 +0300

    Committed 2023 0Tu.145627

--- Parser/parser.py
+++ Parser/parser.py
@@ -605,7 +605,7 @@ def parse_program(tokens, postion):
                         if current_token < len(tokens) and tokens[current_token][0] != "SEMICOLON":
                             print("Syntax Error: statement terminator missing")
                             print(f"VARIABLE ASSIGNMENT: {name} {asg} {express}  <missing ';'>")
-                        else:
+                        elif current_token < len(tokens) and tokens[current_token][0] == "SEMICOLON":
                             print(f"VARIABLE ASSIGNMENT: {name} {asg} {express} {tokens[current_token][1]}")
                     else:
                         print("Syntax Error: variable assignment error, no value was assigned ", {tokens[current_token-1][2]} )
