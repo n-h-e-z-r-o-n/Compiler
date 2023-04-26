3c1c8c2daf35c36fa84b2026c89424310cabb841
Hezron Na <hezron.w12@gmail.com>
2023-04-26 23:40:11 +0300

    Committed 2023 0We.234011

--- Parser/program.c
+++ Parser/program.c
@@ -1,29 +1,52 @@
 
 
-int addition(int a, ){
+int addition(int a, int b){
 
 }
 int subtraction(int a, int b){
 
 }
+float multiplication(float a, float b){
+int result = a * b;
+return result;
+}
+float division(float a, float b){
+int result = a / b;
+return result;
+}
+char boolCheck(int a){
+    char mychar;
+   if(a == 1){
+      mychar = 'T';
+      return mychar;
+   }
+   else{
+      mychar = 'F';
+      return mychar;
+    }
+}
 
-/*
+void main(){
+bool state = true;
+   int c = 3;
+int d = 5;
+float e = 6.0;
+float f = 12.0;
+float newResult1;
+int newResult2;
 
-     if token[current_token + 1][0] == 'COdMMA':
-                        if token[current_token + 2][0] != 'RIGHT_PAREN':
-                            parame +=  token[current_token + 1][1] + ' '
-                            current_token += 1
-                        else:
-                            parame += "  <error-remove ','> "
-                            print("syntax error: ',' error in parameters  at line", token[current_token + 2][2])
-                            current_token += 1
-                    elif token[current_token + 1][0] == 'KEYWORD' or token[current_token + 1][0] == 'IDENTIFIER':
-                        parame += "  <missing ','>  " + token[current_token + 1][1] + ' '
-                        current_token += 1
-                    else:
-                        if token[current_token + 1][0] == 'RIGHT_PAREN':
-                            parame += "  <remove ','>  "
-                     else:
-                    print("Error Expected IDENTIFIER")
+int select = 3;
+while (state){
+       if(select == 1){
+            newResult1 = division(f, e);
+       }else if (select == 2){
+            newResult1 = multiplication(f, e);
+       }else if (select == 3){
+            newResult2 = addition(c, d);
+       }else{
+            newResult2 = subtraction(c, d);
+       }
+       state = false;
+}
+}
 
-*/
