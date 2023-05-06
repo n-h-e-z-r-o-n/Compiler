a4a45e280c679afd91d314629148a13e2af2deb6
Hezron Na <hezron.w12@gmail.com>
2023-05-07 00:10:04 +0300

    Committed 2023 0Su. 01004

--- Parser/parser.py
+++ Parser/parser.py
@@ -707,10 +707,9 @@ print("\n\n======================================== PARSER OUTPUT ==============
 parse_program(tokens, 0)  # calling the parser function and passing token list
 End_run_time_time = time.time()  # Record the End run time-time of lexical_analyzer
 Program_Run_time = End_run_time_time - start_run_time_time  # Calculate the elapsed time (run time of parser function)
-print("\n\n================================================================================================")
 print(f"\nParser Program Runtime  :  {Program_Run_time} seconds")
 
-start_run_time_time = time.time()  # Record the Start run time-time of lexical_analyzer
+
 print("\n===============================  ERROR ================ \n")
 print(Error_list)
 print("\n===============================  PERSER LIST ================ \n")
