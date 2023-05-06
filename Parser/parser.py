9c851cebdcc5d0c3285276c775a3565d895ca344
Hezron Na <hezron.w12@gmail.com>
2023-05-07 01:44:32 +0300

    Committed 2023 0Su. 14432

--- Parser/parser.py
+++ Parser/parser.py
@@ -798,6 +798,7 @@ def Intemidiet_Code_Generator(list_of_tuples):
                         temp.append(child)
                         Intemidiet_Code_Generator(temp)
                         temp = []
+                        print(f"goto L1")
                 if child[0] == 'return_statement':
                      print(f"L2:  return {child[1]}")
 
