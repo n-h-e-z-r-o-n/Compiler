0650221c821eb9b8ff15c4a43886b6064cfd1d98
Hezron Na <hezron.w12@gmail.com>
2023-05-07 01:28:26 +0300

    Committed 2023 0Su. 12825

--- Parser/program.c
+++ Parser/program.c
@@ -1,4 +1,9 @@
-bool state = true;
+
+float division(float a, float b){
+int result = a / b;
+return result;
+}
+
 while (state){
 int c = 3;
 int d = 5;
@@ -6,5 +11,4 @@ float e = 6.0;
 float f = 12.0;
 float newResult1;
 int newResult2;
-
 }
