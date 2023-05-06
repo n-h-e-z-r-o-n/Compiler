77bbbb225142896193001609797d7f389b2a07e2
Hezron Na <hezron.w12@gmail.com>
2023-05-06 16:08:47 +0300

    Committed 2023 0Sa.160847

--- Parser/parser.py
+++ Parser/parser.py
@@ -724,7 +724,7 @@ def Intemidiet_Code_Generator(list_of_tuples):
                 elif child[0] == 'function_parameter':
                     for child in child[1]:
                         for child in child:
-                           print(child)
+                           print(child[1])
                 elif child[0] == 'function_body':
                     for child in child[1]:
                         temp.append(child)
