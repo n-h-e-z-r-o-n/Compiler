b8952b90f351d2ce87dbd382a411da0166f3a07c
Hezron Na <hezron.w12@gmail.com>
2023-05-06 23:31:18 +0300

    Committed 2023 0Sa.233118

--- Parser/program.c
+++ Parser/program.c
@@ -1,17 +1,4 @@
-int addition(int a, int b){
-int result = a + b;
-return result;
-}
 
-int subtraction(int c, int d){
-int result = c - d;
-return result;
-}
-
-float multiplication(float a, float b){
-int result = a * b;
-return result;
-}
 while (state){
 
 }
