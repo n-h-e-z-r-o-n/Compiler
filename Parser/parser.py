45757262c8437db4fb087220a7dfc5b2b080e5a8
Hezron Na <hezron.w12@gmail.com>
2023-05-06 11:51:32 +0300

    Committed 2023 0Sa.115132

--- Parser/parser.py
+++ Parser/parser.py
@@ -405,11 +405,11 @@ def expression(tokens, position):
             if (tokens[current_token + 1][0] != 'SEMICOLON') and (tokens[current_token + 1][0] != 'KEYWORD'):
                 express_n += token_value + ' '
             else:
-                Error_list += f"\nSyntax error -- Expression Error Cause:  {tokens[current_token][1]}  : at line  {tokens[current_token][2]}"
+                Error_list += f"\nSyntax error -- Expression Error Caused by  {tokens[current_token][1]}  : at line  {tokens[current_token][2]}"
         elif token_type == 'SEMICOLON' or token_type == 'RIGHT_PAREN' or token_type == 'EQUAL' or token_type == 'NOT_EQUAL' or token_type == 'LESS_THAN' or token_type == 'GREATER_THAN' or token_type == 'LESS_THAN_EQUAL' or token_type == 'GREATER_THAN_EQUAL':
             break
         else:
-            Error_list += f"\nSyntax error -- Expression Error Cause:  {tokens[current_token][1]}  : at line  {tokens[current_token][2]}"
+            Error_list += f"\nSyntax error -- Expression Error Caused by  {tokens[current_token][1]}  at line  {tokens[current_token][2]}"
             break
         current_token += 1
 
