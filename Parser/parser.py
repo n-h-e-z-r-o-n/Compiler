44a1ae2abf3cd6ab6b61c47d27cd75c664c49afb
Hezron Na <hezron.w12@gmail.com>
2023-05-06 15:46:09 +0300

    Committed 2023 0Sa.154609

--- Parser/parser.py
+++ Parser/parser.py
@@ -17,7 +17,7 @@ def parameter_RFC(token, current_token):
             if token[current_token][0] == 'KEYWORD':
                 if token[current_token + 1][0] == 'IDENTIFIER':
                     parame += token[current_token][1] + ' ' + token[current_token + 1][1] + ' '
-                    node.append(('type_specifier', f'{token[current_token][1]}'))
+                    node.append(('type_specifier', f'{token[current_token][1]}', ("IDENTIFIER", f"{token[current_token + 1][1]}") ))
                     current_token += 1
                     if token[current_token + 1][0] == 'COMMA':
                         if token[current_token + 2][0] != 'RIGHT_PAREN':
@@ -29,6 +29,7 @@ def parameter_RFC(token, current_token):
 
             elif token[current_token][0] == 'IDENTIFIER':
                 parame += token[current_token][1] + ' '
+                node.append(('type_specifier', f'{token[current_token][1]}'))
                 if token[current_token + 1][0] == 'COMMA':
                     if token[current_token + 2][0] != 'RIGHT_PAREN':
                         parame += token[current_token + 1][1] + ' '
