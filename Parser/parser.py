6b5b69dd6dc4bce99452a5d5356fcf224228be8d
Hezron Na <hezron.w12@gmail.com>
2023-05-06 23:47:22 +0300

    Committed 2023 0Sa.234722

--- Parser/parser.py
+++ Parser/parser.py
@@ -52,13 +52,16 @@ def parameter_RFC(token, current_token):
 
 def condition_statement_RFC(tokens, position):
     global express_n
+    node = []
     condition_statment = ''  # store condition statements
     current_token = position
-    current_token, left_operand, exp_node= expression(tokens, current_token)
+    current_token, left_operand, exp_node = expression(tokens, current_token)
     condition_statment += left_operand
+    node.append(tuple(exp_node))
     express_n = ''
     if current_token < len(tokens) and (tokens[current_token][0] == 'EQUAL' or tokens[current_token][0] == 'NOT_EQUAL' or tokens[current_token][0] == 'LESS_THAN' or tokens[current_token][0] == 'GREATER_THAN' or tokens[current_token][0] == 'LESS_THAN_EQUAL' or tokens[current_token][0] == 'GREATER_THAN_EQUAL'):
         conditional_operator = tokens[current_token][1]
+        node.append(tuple(exp_node))
         condition_statment += " " + conditional_operator
         current_token, right_operand, exp_node= expression(tokens, current_token)
         condition_statment += " " + right_operand
