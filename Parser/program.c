2211a055bfbc3b7f1aa5e5b04e5a3cdfa3453e32
Hezron Na <hezron.w12@gmail.com>
2023-05-07 19:52:40 +0300

    Committed 2023 0Su.195240

--- Parser/program.c
+++ Parser/program.c
@@ -1,3 +1,10 @@
-int num;
-num = 0+nu ;
-int tun = 34;
+       if(select == 1){
+            newResult1 = division(f, e);
+       }else if (select == 2){
+            newResult1 = multiplication(f, e);
+       }else if (select == 3){
+            newResult2 = addition(c, d);
+       }else{
+            newResult2 = subtraction(c, d);
+       }
+       state = false;
