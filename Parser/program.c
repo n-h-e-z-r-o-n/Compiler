1b33e592c1b9ef41b621fdba803f829cf87bed26
Hezron Na <hezron.w12@gmail.com>
2023-05-07 22:24:16 +0300

    Committed 2023 0Su.222416

--- Parser/program.c
+++ Parser/program.c
@@ -1,6 +1,14 @@
        int select = 3;
        bool state = true;
+       
        if(select == 1){
             newResult1 = division(f, e);
-       }
+       }else if (select == 2){
+            newResult1 = multiplication(f, e);
+       }else if (select == 3){
+            newResult2 = addition(c, d);
+       }else{
+            newResult2 = subtraction(c, d);
+
+
        state = false;
