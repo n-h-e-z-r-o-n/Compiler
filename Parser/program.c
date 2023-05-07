162c3f7d9a36e9fa9effd66cab4fbe3d3dc6e69b
Hezron Na <hezron.w12@gmail.com>
2023-05-07 14:51:57 +0300

    Committed 2023 0Su.145157

--- Parser/program.c
+++ Parser/program.c
@@ -3,3 +3,15 @@ bool state = true;
 int fume = 56 + 5664;
 
 int tyu  = fume;
+while (state){
+       if(select == 1){
+            newResult1 = division(f, e);
+       }else if (select == 2){
+            newResult1 = multiplication(f, e);
+       }else if (select == 3){
+            newResult2 = addition(c, d);
+       }else{
+            newResult2 = subtraction(c, d);
+       }
+       state = false;
+}
