93aece557495d3da14a3520fccda65afe5c38605
Hezron Na <hezron.w12@gmail.com>
2023-05-07 22:19:47 +0300

    Committed 2023 0Su.221947

--- Parser/program.c
+++ Parser/program.c
@@ -4,8 +4,6 @@
             newResult1 = division(f, e);
        }else if (select == 2){
             newResult1 = multiplication(f, e);
-       }else if (select == 3){
-            newResult2 = addition(c, d);
        }else{
              newResult2 = btrer(c, d);
        }
