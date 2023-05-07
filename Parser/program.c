1e18fc7e2246c3246e07678d8acd29d83ad25af0
Hezron Na <hezron.w12@gmail.com>
2023-05-07 21:54:43 +0300

    Committed 2023 0Su.215443

--- Parser/program.c
+++ Parser/program.c
@@ -2,11 +2,5 @@
        bool state = true;
        if(select == 1){
             newResult1 = division(f, e);
-       }else if (select == 2){
-            newResult1 = multiplication(f, e);
-       }else if (select == 3){
-            newResult2 = addition(c, d);
-       }else{
-
        }
        state = false;
