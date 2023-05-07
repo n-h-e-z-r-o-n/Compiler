0838f315b2f10265fc020eaa2f6d72e898479097
Hezron Na <hezron.w12@gmail.com>
2023-05-08 00:12:44 +0300

    Committed 2023 0Mo. 01244

--- Parser/program.c
+++ Parser/program.c
@@ -1,8 +1,6 @@
-int select = 3;
+
 int state = false;
-while (state){
-     if(num){ int state = false; }
-}
 
 
-//('if_statment', (('if_condition', (('num',),)), ('if_body', (('INITIALIZATION', ('type_specifier', 'int'), ('IDENTIFIER', 'state'), ('expression', ('false',))),))))
+/* ('if_statment', (('if_condition', (('num',),)), ('if_body', (('INITIALIZATION', ('type_specifier', 'int'), ('IDENTIFIER', 'state'), ('expression', ('false',))),))))
+('INITIALIZATION', ('type_specifier', 'int'), ('IDENTIFIER', 'state'), ('expression', ('false',))) */
