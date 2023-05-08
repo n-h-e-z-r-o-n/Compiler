d3af190ae6c3ab3e3b9d9551a853438f5438fad6
Hezron Na <hezron.w12@gmail.com>
2023-05-08 13:57:33 +0300

    Committed 2023 0Mo.135733

--- Parser/parser.py
+++ Parser/parser.py
@@ -750,7 +750,7 @@ print(parser_tree)  # print out the parser tree in list for that will be passed 
 print("\n===============================  Intimidate_Code_Generator ================ \n")
 
 
-def serach(my_dict, target_value):
+def serach(my_dict, target_value): # function that is used to search for same information in our disct dictionary
     for key, value in my_dict.items():
         if value[1] == target_value:
             return key
