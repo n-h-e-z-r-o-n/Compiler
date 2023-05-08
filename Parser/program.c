7f3d1081aa64f092c86960ed410097fd964d111e
Hezron Na <hezron.w12@gmail.com>
2023-05-08 13:04:07 +0300

    Committed 2023 0Mo.130407

--- Parser/program.c
+++ Parser/program.c
@@ -1,10 +1,12 @@
- int a;
- char mychar;
- if(a == 1){
-      mychar = 'T';
-      return mychar;
+ char boolCheck(int a){
+    char mychar;
+   if(a == 1){
+
+       mychar = 'T';
+       return mychar;
   }
    else{
       mychar = 'F';
       return mychar;
     }
+}
