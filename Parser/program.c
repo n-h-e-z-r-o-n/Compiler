0a12f3586b95addab64c1583a26969eefd5c1d5b
Hezron Na <hezron.w12@gmail.com>
2023-05-08 11:15:29 +0300

    Committed 2023 0Mo.111529

--- Parser/program.c
+++ Parser/program.c
@@ -2,7 +2,7 @@ int addition(int a, int b){
 int result = a + b;
 return result;
 }
-int subtraction(int a, int b){
-int result = a - b;
+int subtraction(int m, int f){
+int result = m - f;
 return result;
 }
