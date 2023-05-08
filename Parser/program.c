8771fe8d120a9849c797e46a2b2f143f00e4747f
Hezron Na <hezron.w12@gmail.com>
2023-05-08 11:55:47 +0300

    Committed 2023 0Mo.115547

--- Parser/program.c
+++ Parser/program.c
@@ -1,27 +1,8 @@
-int addition(int a, int b){
-int result = a + b;
-return result;
-}
-int subtraction(int m, int f){
-int result = m - f;
-return result;
-}
-float multiplication(float a, float b){
-int result = a * b;
-return result;
-}
-float division(float a, float b){
-int result = a / b;
-return result;
-}
-char boolCheck(int a){
-    char mychar;
    if(a == 1){
-      mychar = 'T';
       return mychar;
-   }
+       mychar = 'T';
+  }
    else{
       mychar = 'F';
       return mychar;
     }
-}
