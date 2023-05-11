9990c17e27f6fbce72aff82658c552599c98877b
Hezron Na <hezron.w12@gmail.com>
2023-05-11 12:00:48 +0300

    Committed 2023 0Th.120047

--- Parser/parser.py
+++ Parser/parser.py
@@ -979,7 +979,8 @@ def Intemidiet_Code_Generator(parser_tree):
                             for i in x:
                                 t = serach(disct, i)
                                 if disct[t][2] == "true":
-                                    vae += f"( NOT, {t_v}," + t + ")"
+                                    t_V = f"t{count}"
+                                    vae += f"( NOT, {t_V}," + t + ")"
                                 elif disct[t][2] == "false":
                                     vae += " NOT  " + t + " "
                                 else:
@@ -997,9 +998,9 @@ def Intemidiet_Code_Generator(parser_tree):
                             vae += x
 
                     l = label_track
-                    t_V = f"t{count}"
+
                     # print(f"{t_V} = {vae}")  # (NOT, t2, t1)
-                    print(f"({vae})")
+                    print(f"{vae}")
 
                     count+=1
                     #print(f"L{l} : if ( {t_V} ) goto L{r}")
