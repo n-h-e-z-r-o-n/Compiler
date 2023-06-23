9c2da503e1e28154346c7b1d24904998cfb8a098
Hezron Na <hezron.w12@gmail.com>
2023-06-23 15:58:20 +0300

    Committed 2023 0Fr.155820

--- main.py
+++ main.py
@@ -85,8 +85,8 @@ def colorize_text(event):
         for match in re.finditer(r"[\"][^']*[\"]", text):
             start = match.start()
             end = match.end()
-            Editor.tag_add("gray", f"1.0+{start}c", f"1.0+{end}c")
-            Editor.tag_config("gray", foreground="gray")
+            Editor.tag_add("green", f"1.0+{start}c", f"1.0+{end}c")
+            Editor.tag_config("green", foreground="gr")
 
         # Find and colorize strings
         for match in re.finditer(r'(\/\/[^\n\r]*[\n\r])|\/\*[\s\S]*?\*\/', text):
