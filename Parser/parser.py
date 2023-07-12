d72e595c19b847ce5710209fcaafd47089761a3b
Hezron Na <hezron.w12@gmail.com>
2023-07-12 20:30:01 +0300

    Committed 2023 0We.203001

--- Parser/parser.py
+++ Parser/parser.py
@@ -923,8 +923,9 @@ def parse_program(tokens, postion):
                                 while True:
                                     if tokens[current_token][0] == "IDENTIFIER":
                                         structure_variable = tokens[current_token][1]
-                                        current_token += 1
                                         print("--------", tokens[current_token])
+                                        current_token += 1
+
                                         if (current_token + 1) < len(tokens) and tokens[current_token + 1][0] == "SEMICOLON":  # handle declaration
                                             print(f"DECLARATION :  --- {structure_variable}")
                                             parser_tree.append(("STRUCTURE_VARIABLE", ('structure_name', structure_name), ('structure_variable', structure_variable)))
