3ed292723e02ce74bcf507be131b2b9a550b17a8
Hezron Na <hezron.w12@gmail.com>
2023-07-13 12:52:51 +0300

    Committed 2023 0Th.125251

--- Parser/parser.py
+++ Parser/parser.py
@@ -1000,7 +1000,7 @@ def parse_program(tokens, postion):
 
         elif tokens[current_token][0] == 'ENUMERATION_KEY':  # enumeration
             if (current_token + 1) < len(tokens) and tokens[current_token + 1][0] == 'ASSIGN':
-                if (current_token + 2) < len(tokens) and (tokens[current_token + 2][0] == 'IDENTIFIER' or tokens[current_token + 2][0] == 'STRING' or tokens[current_token + 2][0] == 'CHAR' or tokens[current_token + 2][0] == 'FLOATING_POINT' or tokens[current_token + 2][0] == 'INTEGER'):
+                if (current_token + 2) < len(tokens) and tokens[current_token + 2][0] == 'IDENTIFIER':
                     parser_tree.append(("STRUCTURE_MEMBER_ACCESS", ("structure_member_access_ame", tokens[current_token][1]), ('assigned_valued', (tokens[current_token + 2][1]))))
                     current_token += 2
                     if (current_token + 1) < len(tokens) and tokens[current_token + 1][0] == 'SEMICOLON':
