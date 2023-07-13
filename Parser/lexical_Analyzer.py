dd09b7d41aaffd4c91acf281d402615752b38866
Hezron Na <hezron.w12@gmail.com>
2023-07-13 12:36:30 +0300

    Committed 2023 0Th.123629

--- Parser/lexical_Analyzer.py
+++ Parser/lexical_Analyzer.py
@@ -15,6 +15,7 @@ patterns_rg = [
     (r'#pragma\b', 'PRAGMA_DIRECTIVE'),
     (r'\b(int|void|char|double|bool|float|long|return)\b', 'KEYWORD'),
     (r'\bstruct\b', 'STRUCT_KEY'),
+    (r'\benum\b', 'ENUMERATION_KEY'),
 
     (r'\b(const)\b', 'CONSTANT_KEY'),
     (r'[a-zA-Z_][a-zA-Z0-9_]*\[(\s*[a-zA-Z_][a-zA-Z0-9_]*\s*|\s*[0-9]+\s*)*\s*\]', 'ARRAY'),
