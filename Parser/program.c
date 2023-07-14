833e4b9321c99626ab1ea322c4d3fe01f6395fc8
Hezron Na <hezron.w12@gmail.com>
2023-07-14 11:21:02 +0300

    Committed 2023 0Fr.112102

--- Parser/program.c
+++ Parser/program.c
@@ -2,18 +2,11 @@
 
 
     if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
-        printf("\nInvalid address/ Address not supported \n");
+
         return -1;
     }
 
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\nConnection Failed \n");
+       
         return -1;
     }
-    send(sock , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
-    valread = read( sock , buffer, 1024);
-    printf("%s\n",buffer );
-    return 0;
-}
-*/
