97154647ead8eddf9c2ddcef4142e4a07591ba02
Hezron Na <hezron.w12@gmail.com>
2023-07-14 11:16:57 +0300

    Committed 2023 0Fr.111657

--- Parser/program.c
+++ Parser/program.c
@@ -1,19 +1,6 @@
 
 
-int main(int argc, const char ) {
-    int sock = 0, valread;
-    struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
-    char buffer[1024] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-
-    // Convert IPv4 and IPv6 addresses from text to binary form
     if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
